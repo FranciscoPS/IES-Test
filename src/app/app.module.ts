2c2f0cc877e54de219f0e2a2b3f4c5dfa2579225
@francisco.peralta1 <francisco.peralta@umvel.com>
2022-12-16 16:29:08 -0600

    feat: navbar implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/modules/material.module';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent
   ],
   imports: [
     AppRoutingModule,
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/shared/modules/material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     LoginRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MaterialModule
   ]
 })
 export class LoginModule { }
