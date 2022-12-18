67ccc5cb0cbcf2f1dc12f4d4116ebe27bbfdeebf
@francisco.peralta1 <francisco.peralta@umvel.com>
2022-12-18 12:46:30 -0600

    feat: cambioLetras Pipe Implemented

---
+++ src/app/shared/pipes/cambioLetras.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { VOWELS } from '@constants';
+
+@Pipe({
+  name: 'cambioLetras',
+})
+export class CambioLetrasPipe implements PipeTransform {
+  transform(value: string): string {
+    const newString = value.replace(/[aáAÁeéEÉiíIÍoOóÓuúUÚ]/g, (letter) => VOWELS[letter]);
+
+    return newString;
+  }
+}
